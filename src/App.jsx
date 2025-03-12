4594efd2b0b2e7a0a0944d4dbfc64c5dd3ae2062
IkuroMaira <gwen.bussac@gmail.com>
2025-03-12 14:42:50 +0100

    Add description accordion components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './styles/App.css'
+import './styles/Global.css'
+import ProductDetails from "./pages/ProductDetailsPart2.jsx";
 
 function App() {
 
   return (
     <>
+        {/*TODO : créer un bouton pour simuler le renvoie sur la page*/}
+        <ProductDetails />
     </>
   )
 }
---
+++ src/pages/ProductDetailsPart2.jsx
@@ -0,0 +1,11 @@
+import AccordeonSizes from "../components/AccordeonSizes.jsx";
+import AccordeonDelivery from "../components/AccordeonLivraison.jsx";
+
+export default function ProductDetails() {
+    return (
+        <>
+            <AccordeonSizes />
+            <AccordeonDelivery />
+        </>
+    )
+}
