f2d1d3c5c5a4a406994d50b6ba9ac716373ae455
Ludo <ludovic.pouet@gmail.com>
2025-03-13 16:17:15 +0100

    update product page

---
+++ src/components/product/Accordion.jsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+import '../../styles/detailsProduct/accordeonDelivery.css';
+import { FaChevronUp } from "react-icons/fa";
+
+export default function AccordeonDelivery() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleAccordeon() {
+        // Ici on change le statut de 'isOpen' quand on clique
+        setIsOpen(!isOpen)
+        console.log(state);
+    }
+
+    return (
+        <>
+            {/*HTML pour le composant livraison accordéon*/}
+            <div className="accordion">
+                <div className="accordion-header" onClick={toggleAccordeon}>
+                    <h2 id="accordion-title">
+                        Livraison
+                    </h2>
+                    {/*Icône qui va changer selon l'état*/}
+                    <div className={`accordion-icon ${isOpen ? 'open' : ''}`}>
+                        <FaChevronUp />
+                    </div>
+                </div>
+
+                {/*Le contenu de la livraison*/}
+                {isOpen && (
+                    <div className="accordion-content">
+                        <p>Vous pouvez vous faire livrer par Colissimo ou en Point Relay.</p>
+                    </div>
+                )}
+            </div>
+        </>
+    );
+}
---
+++ src/pages/Product.jsx
@@ -0,0 +1,15 @@
+import Accordion from "./Accordion.jsx";
+
+export default function Dimensions() {
+    return (
+        <Accordion title="Dimensions">
+            {/* Contenu personnalisé */}
+            <p><strong>Largeur : </strong>68 cm</p>
+            <p><strong>Profondeur : </strong>82 cm</p>
+            <p><strong>Hauteur : </strong>100 cm</p>
+            <p><strong>Larg. assise : </strong>56 cm</p>
+            <p><strong>Prof. assise : </strong>50 cm</p>
+            <p><strong>Haut. assise : </strong>42 cm</p>
+        </Accordion>
+    );
+}
