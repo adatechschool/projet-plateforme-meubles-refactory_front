32783082f330a19e0cc682a721bd62975c0e4028
stellab95 <stellab95@hotmail.fr>
2025-03-13 11:12:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,33 @@ const router = createBrowserRouter([
   {
     path: '/detailcommand',
     element: <DetailCommand />
-  }
+  },
+  {
+    path: '/detailcommand',
+    element: <Banner />
+  },
+  {
+    path: '/detailcommand',
+    element: <Background />
+  },
 ])
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Accueil from './components/accueil/Accueil'
+import Banner from './components/Banner'
+import Background from './components/Background'
+import FurnitureFilter from './components/accueil/FurnitureFilter'
 import Card from './components/accueil/Card';
 import Footer from './components/accueil/Footer'
-import FurnitureFilter from './components/accueil/FurnitureFilter'
 import DetailCommand from './components/DetailCommand'
 
 import './styles/Card.css'
 import './styles/Footer.css'
 import './styles/App.css'
+import './styles/Banner.css'
 import './styles/FurnitureFilter.css'
 import './styles/DetailCommand.css'
+import './styles/Background.css'
 
 function App() {
   return <RouterProvider router={router} />
--- src/components/accueil/Accueil.jsx
+++ src/components/accueil/Accueil.jsx
@@ -1,10 +1,14 @@
-import FurnitureFilter from './FurnitureFilter'
+import Banner from '../Banner';
+import Background from '../Background';
+import FurnitureFilter from './FurnitureFilter';
 import Card from './Card';
-import Footer from './Footer'
+import Footer from './Footer';
 
 function Accueil() {
     return (
       <div>
+        <Banner />
+        <Background />
         <FurnitureFilter />
         <Card />
         <Footer />
