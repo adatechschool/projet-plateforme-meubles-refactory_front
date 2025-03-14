f7b3d13a908cc30d444946749ab0e414f33de914
Ludo <ludovic.pouet@gmail.com>
2025-03-14 14:20:05 +0100

    add Product path

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Accueil from './components/accueil/Accueil';
 import Panier from "./components/Panier";
 
-
-import './styles/Card.css'
 import './styles/Footer.css'
-import './styles/Banner.css'
 import './styles/FurnitureFilter.css'
 import './styles/DetailCommand.css'
 import './styles/Background.css'
+import Product from "./components/Product.jsx";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: '/panier',
     element: <Panier/>
+  },
+  {
+    path: '/Product',
+    element : <Product/>
   }
 ])
 
