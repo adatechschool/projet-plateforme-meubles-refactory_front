526291679e2c827ac7936c7496bfe01cd2d24350
Ludo <ludovic.pouet@gmail.com>
2025-03-14 09:58:08 +0100

    git fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-
-import Acceuil from "./components/acceuil/Acceuil";
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Acceuil />
-  }
+    element: <Accueil />
+  },
+  {
+    path: '/detailcommand',
+    element: <DetailCommand />
+  },
 ])
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Accueil from './components/accueil/Accueil'
+import Banner from './components/Banner'
+import Background from './components/Background'
+import FurnitureFilter from './components/accueil/FurnitureFilter'
+import Card from './components/accueil/CardList';
+import Footer from './components/accueil/Footer'
+import DetailCommand from './components/DetailCommand'
 
+import './styles/Card.css'
 import './styles/Footer.css'
 import './styles/Banner.css'
 import './styles/FurnitureFilter.css'
 import './styles/DetailCommand.css'
 import './styles/Background.css'
-import Accueil from "./components/accueil/Accueil.jsx";
 
 function App() {
   return <RouterProvider router={router} />
