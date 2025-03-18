8dd440ae801ece0ff419d535c097de11c4e85a7f
Marie Marchal <you@example.com>
2025-03-18 15:07:53 +0100

    filtre par catégories bannière

--- src/components/accueil/FurnitureFilter.jsx
+++ src/components/accueil/FurnitureFilter.jsx
@@ -1,18 +1,19 @@
 function FurnitureFilter(){
     return (
             <div className="filter-container">
-                <h2 className="tendances">Dernières tendances</h2>
+                <h3 className="tendances">Filtrer par catégories</h3>
 
-                <ul className="filter-content">
+                {/* <ul className="filter-content">
                     <li><a>Chaises</a></li>
                     <li><a>Armoires</a></li>
                     <li><a>Canapés</a></li>
                     <li><a>Tables</a></li>
                     <li><a>Lampes</a></li>
                     <li><a>Accessoires</a></li>
-                </ul>
+                </ul> */}
             </div>
     );
 }
 
 export default FurnitureFilter;
+
