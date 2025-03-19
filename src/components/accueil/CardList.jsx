2ce98f6e5b7ff4ffbfc2707f6ffeedca2cf9378d
stellab95 <stellab95@hotmail.fr>
2025-03-19 12:11:11 +0100

    ajout bouton voir plus cardsList

--- src/components/accueil/CardList.jsx
+++ src/components/accueil/CardList.jsx
@@ -2,6 +2,13 @@ import { useState, useEffect } from 'react';
 import Card from './Card';
 
 function CardList() {
+  const [visibleArticles, setVisibleArticles] = useState(6);
+
+  // Fonction pour afficher 6 articles de plus
+  const handleShowMore = () => {
+    setVisibleArticles((prev) => prev + 6);
+  };
+
   const [products, setProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(""); // État pour la catégorie sélectionnée
 
@@ -47,10 +54,16 @@ function CardList() {
       {/* Liste des produits filtrés */}
       <div className="center-cards">
         <div className="container-all-cards">
-          {products.map((product) => (
-            <Card key={product.id} product={product} />
-          ))}
-        </div>
+
+        {products.slice(0, visibleArticles).map((product) => (
+        <Card key={product.id} product={product} />
+      ))}        
+      </div>
+      </div>
+      <div className='container-show-more-button'>
+      {visibleArticles < products.length && (
+        <button onClick={handleShowMore} className="show-more-button">Voir plus</button>
+      )}
       </div>
     </div>
   );
